7872839b40dbaa92483d4d78a0c280970d113ba4
Le Van Tung <letung131297@gmail.com>
2018-05-28 14:33:37 +0700

    thay đổi giao diện menu bar, thêm font-awesome

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="<?= base_url() ?>public/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>public/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= base_url() ?>public/css/app.css">
 </head>
 <body>
     
@@ -17,42 +19,49 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Quản lí hồ sơ sinh viên</a>
+                    <a class="navbar-brand" href="<?= base_url() ?>index.php/userController/home">
+                        <i class="fa fa-graduation-cap"></i>&nbsp;Quản lí hồ sơ sinh viên
+                    </a>
                 </div>
         
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse navbar-ex1-collapse">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="">Home</a></li>
-                        <li><a href="#">Link</a></li>
+                        <li class="active"><a href=""><i class="fa fa-home"></i> &nbsp;Home</a></li>
+                        <!-- <li><a href="#">Link</a></li> -->
                     </ul>
                     <form class="navbar-form navbar-left" role="search">
                         <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Search">
+                            <input type="text" class="form-control" placeholder="Từ khóa ...">
                         </div>
-                        <button type="submit" class="btn btn-success">Submit</button>
+                        <button type="submit" class="btn btn-success">&nbsp;<i class="fa fa-search"></i>&nbsp;</button>
                     </form>
                     <ul class="nav navbar-nav navbar-right">
-                        <?php if (isset($_SESSION['username'])) {
-                            if ($_SESSION['username'] != '') { ?>
-                                <li><a href="<?= base_url() ?>index.php/userController/logout">Đăng xuất</a></li>
-                                <?php
-                            } else {
-                                ?>
-                                <li><a href="<?= base_url() ?>index.php/userController/login">Đăng nhập</a></li>
-                                <?php
-                            }
-                        } ?>
-                        <?php if (isset($_SESSION['username'])): ?>
-                            
                         
+                        <?php if (isset($_SESSION['username'])): ?>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?= $_SESSION['username'] ?> <b class="caret"></b></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-user"></i> &nbsp;<?= $_SESSION['username'] ?> <b class="caret"></b>
+                            </a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
-                                <li><a href="#">Separated link</a></li>
+                                <li>
+                                    <a href="#"><i class="fa fa-user"></i> &nbsp;Thông tin cá nhân</a>
+                                </li>
+                                <?php if (isset($_SESSION['username'])) {
+                                    if ($_SESSION['username'] != '') { ?>
+                                        <li>
+                                            <a href="<?= base_url() ?>index.php/userController/logout">
+                                                <i class="fa fa-arrow-circle-right"></i>&nbsp;Đăng xuất
+                                            </a>
+                                        </li>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <li><a href="<?= base_url() ?>index.php/userController/login">Đăng nhập</a></li>
+                                        <?php
+                                    }
+                                } ?>
+
                             </ul>
                         </li>
                         <?php endif; ?>
